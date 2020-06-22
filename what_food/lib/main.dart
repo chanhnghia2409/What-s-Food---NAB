d90aa31814a45392a126f872882511ca74229de5
HerooNAB <heroonab@github.com>
2020-06-22 23:10:51 +0700

    binh xoa 3 dong
    22/06/2020 binh update

--- what_food/lib/main.dart
+++ what_food/lib/main.dart
@@ -6,9 +6,6 @@ import 'package:what_food/Screens/Welcome/welcome_screen.dart';
 void main() => runApp(MyApp());
 
 
-
-
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
